d3e1a450168f940487b1c6e9b5ec48f1b0f2fa95
Alan-turing-mathematician <mikhail.motuzny@gmail.com>
2020-10-01 18:01:07 +0300

    'almost_final'

--- AntHill/urls.py
+++ AntHill/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('logout/', AuthViews.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', UserRegisterPage.as_view(template_name='register.html'), name='register'),
     path('create/', PostCreateView.as_view(template_name='create_post.html'), name='create_post'),
-    path('profile/', UserProfilePage, name='profile'),
+    path('profile_my/', UserProfilePage, name='profile'),
     path('post/<id>/', PostSingle, name='post_single'),
     path('author/<id>/', PostAuthorProfileView, name='post_author'),
     path('video/<id>/', VideoSingle, name='video_single'),
