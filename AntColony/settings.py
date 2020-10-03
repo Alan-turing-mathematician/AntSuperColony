5d6a960fdba6d454de66a69565ec86c85521756e
Alan <mikhail.motuzny@gmail.com>
2020-10-03 10:37:22 +0300

    true

--- AntColony/settings.py
+++ AntColony/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'g!$ikw_n21di703u_-&!3zkbpq3=k32=3lk7$15+teoee+39sw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -86,22 +86,22 @@ WSGI_APPLICATION = 'AntColony.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG:
-	DATABASES = {
-    	'default': {
-        	'ENGINE': 'django.db.backends.sqlite3',
-        	'NAME': BASE_DIR / 'db.sqlite3',
-    	}
-	}
+#if DEBUG:
+	#DATABASES = {
+    	#'default': {
+        	#'ENGINE': 'django.db.backends.sqlite3',
+        	#'NAME': BASE_DIR / 'db.sqlite3',
+    	#}
+	#}
 
 
-else:
-	DATABASES = {
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'blogdb',
-        'USER': 'Clay',
-        'PASSWORD': 'starwars2026',
+        'USER': 'postgres',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
