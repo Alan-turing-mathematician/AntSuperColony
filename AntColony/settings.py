af6bab3a5feb91d25ffd4c67629e90179791dfa3
Alan <mikhail.motuzny@gmail.com>
2020-10-03 09:42:06 +0300

    db setup

--- AntColony/settings.py
+++ AntColony/settings.py
@@ -86,12 +86,27 @@ WSGI_APPLICATION = 'AntColony.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+if DEBUG:
+	DATABASES = {
+    	'default': {
+        	'ENGINE': 'django.db.backends.sqlite3',
+        	'NAME': BASE_DIR / 'db.sqlite3',
+    	}
+	}
+
+
+else:
+	DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogbd',
+        'USER': 'Clay',
+        'PASSWORD': 'starwars2026',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
